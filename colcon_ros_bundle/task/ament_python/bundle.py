b0dbb740a78c9918855fdeb7d2192ec870272163
r-meagher <r-meagher@users.noreply.github.com>
2019-09-09 09:35:14 -0700

    Shorten comments to appease Flake

--- colcon_ros_bundle/task/ament_python/bundle.py
+++ colcon_ros_bundle/task/ament_python/bundle.py
@@ -36,8 +36,9 @@ class RosAmentPythonBundleTask(TaskExtensionPoint):
                     'because it is in the workspace'.format_map(locals()))
                 continue
 
-            # Currently, only the first five of these mappings are supported by colcon-core
-            # The others are added for completeness with PEP 440
+            # Currently, only the first five of these mappings are supported
+            # by colcon-core. The others are added for completeness with
+            # PEP 440.
             symbol_mapping = {
                 'version_eq': '==',
                 'version_lte': '<=',
