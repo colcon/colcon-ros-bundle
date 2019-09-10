50204753ad45d3f9572756b70bfc282504bf8e85
Matthew Murphy <matmur@amazon.com>
2019-09-10 10:15:24 -0700

    Support versioned and unversioned dependencies in python packages (#25)
    Update symbol mapping to be compatible with PEP440

--- colcon_ros_bundle/task/ament_python/bundle.py
+++ colcon_ros_bundle/task/ament_python/bundle.py
@@ -36,15 +36,32 @@ class RosAmentPythonBundleTask(TaskExtensionPoint):
                     'because it is in the workspace'.format_map(locals()))
                 continue
 
-            if 'version_eq' in dependency.metadata:
-                pip = args.installers['pip3']
+            # Currently, only the first five of these mappings are supported
+            # by colcon-core. The others are added for completeness with
+            # PEP 440.
+            symbol_mapping = {
+                'version_eq': '==',
+                'version_lte': '<=',
+                'version_gte': '>=',
+                'version_gt': '>',
+                'version_lt': '<',
+                'version_neq': '!=',
+                'version_aeq': '===',
+                'version_compatible': '~=',
+            }
+
+            pip = args.installers['pip3']
+            versioned = False
+            for comparator, version in dependency.metadata.items():
+                if symbol_mapping.get(comparator) is not None:
+                    versioned = True
+                    pip.add_to_install_list(
+                        dependency.name + symbol_mapping[comparator] + version
+                    )
+            if not versioned:
                 pip.add_to_install_list(
-                    dependency.name + '==' + dependency.metadata['version_eq'])
-            else:
-                logger.warning('Currently only support version locked '
-                               'packages. Skipping: {dependency}'
-                               .format(dependency=dependency))
-                continue
+                    dependency.name
+                )
 
             # TODO: The Pip managers should be doing this
             apt = args.installers['apt']
